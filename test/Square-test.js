f14cfe60c6f68a88baef9fabaa6e7e2b99c73c28
Hector Sanchez <hsanchez7934@gmail.com>
2017-08-22 22:07:19 -0600

    refactor square test

--- test/Square-test.js
+++ test/Square-test.js
@@ -1,36 +1,32 @@
 const {assert} = require('chai');
 const Square = require('../lib/Square.js');
 
-describe('Square', function() {
-  it('should take a height', function() {
-    var square = new Square(20, 20, 20, 20, 'blue');
+describe('Square', () => {
 
+  let square;
+
+  beforeEach(() => {
+    square = new Square(20, 20, 20, 20, '#000');
+  });
+
+  it('should take a height', () => {
     assert.equal(square.height, 20);
-  })
+  });
 
-  it('should take a width', function() {
-    var square = new Square(20, 20, 20, 20, 'blue');
-
+  it('should take a width', () => {
     assert.equal(square.width, 20);
-  })
+  });
 
-  it('should take an x coordinate', function() {
-    var square = new Square(20, 20, 20, 20, 'blue');
-
+  it('should take an x coordinate', () => {
     assert.equal(square.x, 20);
-  })
+  });
 
-  it('should take a y coordinate', function() {
-    var square = new Square(20, 20, 20, 20, 'blue');
-
+  it('should take a y coordinate', () => {
     assert.equal(square.y, 20);
-  })
+  });
 
-  it('should draw a square to the canvas', function() {
-    var square = new Square(20, 20, 20, 20, "red");
-    var canvas = document.getElementById('game');
-    var context = canvas.getContext('2d')
-    assert.equal(square.draw(context), true);
+  it('should take a color', () => {
+    assert.equal(square.color, '#000');
+  });
 
-  })
 })
