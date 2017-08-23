8112f4cc034c37a6a45560a43d56f6165ff61c61
Hector Sanchez <hsanchez7934@gmail.com>
2017-08-22 21:55:08 -0600

    update food test

--- test/Segment-test.js
+++ test/Segment-test.js
@@ -8,19 +8,19 @@ describe('Segment', function() {
     segment = new Segment(20, 20, 20, 20, 1);
   });
 
-  it('should take a width', function() {
+  it('should take a width', () => {
     assert.equal(segment.width, 20);
   });
 
-  it('should take a height', function() {
+  it('should take a height', () => {
     assert.equal(segment.height, 20);
   });
 
-  it('should take an x coordinate', function() {
+  it('should take an x coordinate', () => {
     assert.equal(segment.x, 20);
   });
 
-  it('should take a y coordinate', function() {
+  it('should take a y coordinate', () => {
     assert.equal(segment.y, 20);
   });
 
@@ -28,11 +28,11 @@ describe('Segment', function() {
     assert.equal(segment.data, 1);
   });
 
-  it('should start off moving right', function() {
+  it('should start off moving right', () => {
     assert.equal(segment.direction, 'right');
   });
 
-  it('should start off as green in color', function() {
+  it('should start off as green in color', () => {
     assert.equal(segment.color, '#0f0')
   });
 })
